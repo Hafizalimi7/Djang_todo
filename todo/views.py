caac1c15686df63671b9aebb84aabe280e23d630
Hafiz <hafizalimi77@gmail.com>
2024-01-29 22:18:08 +0000

    can now add tasks

--- todo/views.py
+++ todo/views.py
@@ -6,7 +6,10 @@ from django.urls import reverse_lazy
 # Create your views here.
 class Index(ListView):
   template_name = "base.html"
-  queryset = TodoMod.objects.filter(completed=False)
+  queryset = TodoMod.objects.filter(completed=True)
+  todo_names = TodoMod.objects.values_list('completed', flat=True)
+
+  print(todo_names)
   context_object_name = "todo_list"
 
 class AddTask(CreateView):
@@ -21,10 +24,10 @@ class UpdateTask(UpdateView):
   fields = ['completed']
   template_name = "base.html"
   success_url = reverse_lazy("todo:home")
-  
-  
-  
-
+  # def get_queryset(self):
+  #   query_set = TodoMod.objects.filter(completed=True, pk=self.kwargs['pk'])
+  #   print("2", query_set)
+  #   return query_set 
     
 
   
